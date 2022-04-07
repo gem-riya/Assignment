270c371d9cbcd9df393a8947128e4a28c61ed73c
gem-riya <103178346+gem-riya@users.noreply.github.com>
2022-04-07 16:50:49 +0530

    Add files via upload

---
+++ table.js
@@ -0,0 +1,47 @@
+
+// When script is l;loaded function
+// automatically gets called
+yourFunction();
+ 
+function yourFunction(){
+    var users = [
+        { name: "eia", email: "eia@gmail.com" , age: 22, dob: "04/04/2000", company: "abc"},
+        { name: "Ria", email: "ria@gmail.com" , age: 22, dob: "04/09/2000", company: "abc"},
+        { name: "aman", email: "aman@gmail.com" , age: 22, dob: "04/08/2000", company: "abc"},
+        { name: "mina", email: "mi@gmail.com" , age: 22, dob: "04/06/2000", company: "abc"},
+        { name: "sia", email: "sia@gmail.com" , age: 22, dob: "04/04/2000", company: "abc"} 
+    ];
+
+  for (i = 0; i < users.length; i++) {
+    document.write("<tr>");
+
+        document.write("<td class='td'>");
+        document.write(users[i].name);
+        document.write("</td>");
+
+        document.write("<td class='td'>");
+        document.write(users[i].email);
+        document.write("</td>");
+        
+        document.write("<td class='td'>");
+        document.write(users[i].age);
+        document.write("</td>");
+        
+        document.write("<td class='td'>");
+        document.write(users[i].dob);
+        document.write("</td>");
+        
+        document.write("<td class='td'>");
+        document.write(users[i].company);
+        document.write("</td>");
+    
+    document.write("</tr>");
+  }
+}
+ 
+/* <th>Name</th> */
+
+// header.js
+// document.write("<th>")
+// document.write(header[i])
+// document.write("</th>")
